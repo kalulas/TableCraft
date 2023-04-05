88592908eb5d29b238a2429bb91268c6b373ccec
edwardchen <499485532@qq.com>
2023-04-05 17:59:38 +0800

    refactor(library): Remove ConfigInfo caching in ConfigManager

--- ConfigCodeGenLib/ConfigManager.cs
+++ ConfigCodeGenLib/ConfigManager.cs
@@ -48,37 +48,10 @@ namespace ConfigCodeGenLib
         /// </summary>
         public bool ReadComment;
 
-        private readonly Dictionary<string, ConfigInfo> m_ConfigInfoDict = new Dictionary<string, ConfigInfo>();
-
-        #endregion
-
-        #region Private Methods
-
-        private string GetConfigIdentifierInternal(string configFilePath)
-        {
-            return Path.GetFileNameWithoutExtension(configFilePath);
-        }
-
         #endregion
 
         #region Public API
-
-        public string GetConfigIdentifier(string configFilePath)
-        {
-            return GetConfigIdentifierInternal(configFilePath);
-        }
-
-        /// <summary>
-        /// A simple getter
-        /// </summary>
-        /// <param name="identifier"></param>
-        /// <param name="configInfo"></param>
-        /// <returns></returns>
-        public bool GetConfigInfo(string identifier, out ConfigInfo configInfo)
-        {
-            return m_ConfigInfoDict.TryGetValue(identifier, out configInfo);
-        }
-
+        
         /// <summary>
         /// Add new <see cref="ConfigInfo"/> with no json file created
         /// </summary>
@@ -87,14 +60,7 @@ namespace ConfigCodeGenLib
         /// <returns></returns>
         public ConfigInfo AddNewConfigInfo(string configFilePath, EConfigType configType)
         {
-            var identifier = GetConfigIdentifierInternal(configFilePath);
-            if (m_ConfigInfoDict.Remove(identifier))
-            {
-                Debugger.LogWarning("[ConfigManager.AddNewConfigInfo] previous {0} ConfigInfo instance is removed", identifier);
-            }
-            
             var configInfo = ConfigInfoFactory.CreateConfigInfo(configType, configFilePath);
-            m_ConfigInfoDict[identifier] = configInfo;
             return configInfo;
         }
 
@@ -107,32 +73,8 @@ namespace ConfigCodeGenLib
         /// <returns></returns>
         public ConfigInfo AddNewConfigInfo(string configFilePath, string jsonFilePath, EConfigType configType)
         {
-            var identifier = GetConfigIdentifierInternal(configFilePath);
-            if (m_ConfigInfoDict.Remove(identifier))
-            {
-                Debugger.LogWarning("[ConfigManager.AddNewConfigInfo] previous {0} ConfigInfo instance is removed", identifier);
-            }
-
             var configInfo = ConfigInfoFactory.CreateConfigInfo(configType, configFilePath, jsonFilePath);
-            m_ConfigInfoDict[identifier] = configInfo;
             return configInfo;
-        }
-
-        /// <summary>
-        /// Try get ConfigInfo and save to json file
-        /// </summary>
-        /// <param name="identifier"></param>
-        /// <param name="jsonFilePath"></param>
-        /// <returns> true if found and saved </returns>
-        public bool SaveConfigInfoJsonFile(string identifier, string jsonFilePath)
-        {
-            if (!m_ConfigInfoDict.TryGetValue(identifier, out var configInfo))
-            {
-                return false;
-            }
-            
-            configInfo.SaveJsonFile(jsonFilePath);
-            return true;
         }
 
         /// <summary>
